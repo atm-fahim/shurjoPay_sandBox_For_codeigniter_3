438708bdc7ae4cb6626dfd1203eb13c0ff14224a
unknown <atmfahimcse@gmail.com>
2022-03-16 23:47:35 +0600

    shurjopay version two codignator plugin

--- application/libraries/Shurjopay.php
+++ application/libraries/Shurjopay.php
@@ -62,7 +62,7 @@ class Shurjopay {
         curl_setopt($ch, CURLOPT_POST, 1);                //0 for a get request
         curl_setopt($ch, CURLOPT_POSTFIELDS, $xml_data);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 300);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         $response = curl_exec($ch);
         curl_close($ch);
